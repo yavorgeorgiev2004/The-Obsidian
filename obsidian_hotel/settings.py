da73540914d6d213eebe37c3d2d755888181fd15
Yavor <yavkata04@gmail.com>
2026-06-25 12:51:29 +0300

    Change DEBUG setting to False by default

--- obsidian_hotel/settings.py
+++ obsidian_hotel/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ── SECURITY ──────────────────────────────────────────────
 SECRET_KEY = config('SECRET_KEY')
-DEBUG       = config('DEBUG', default=True, cast=bool)
+DEBUG       = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
 # ── APPLICATIONS ──────────────────────────────────────────
